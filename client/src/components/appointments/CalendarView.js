1d23215132c009871a45d713b9c0174b0b3a7992
Susmithagubba17 <sushmithagubba1709@gmail.com>
2026-07-28 16:01:21 +0530

    Update CalendarView.js

--- client/src/components/appointments/CalendarView.js
+++ client/src/components/appointments/CalendarView.js
@@ -4,7 +4,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import axios from 'axios';
-import { useAuth } from '../../context/AuthContext';
+
 
 const CalendarView = () => {
   
